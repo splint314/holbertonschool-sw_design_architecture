ea00aa0c0fee24f5a0542fb0266413d24bbb62b7
splint314 <krigal323@gmail.com>
2026-05-22 23:29:07 +0200

    implement caramel decorator following existing wrapper pattern

---
+++ design_patterns/2-decorator.py
@@ -0,0 +1,9 @@
+class CaramelDecorator:
+    def __init__(self, inner):
+        self._inner = inner
+
+    def cost(self):
+        return self._inner.cost() + 15
+
+    def description(self):
+        return self._inner.description() + " + caramel"
